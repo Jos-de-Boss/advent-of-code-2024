50e3e4030f4d3ff37f4ee66ebd00b9f240e8a5fb
Jos de Boss <s1149817@student.hsleiden.nl>
2024-12-03 19:50:25 +0100

    day-3: solution to puzzle 2

--- day-3-mull-it-over/src/main.rs
+++ day-3-mull-it-over/src/main.rs
@@ -2,23 +2,40 @@ use std::fs;
 use regex::Regex;
 
 const FILE_PATH: &str = "./data";
+const DO_INSTRUCTION: &str = "do()";
+const DONT_INSTRUCTION: &str = "don't()";
 
 fn main() {
     let corrupted_data = fs::read_to_string(FILE_PATH)
         .expect(&format!("Could not read contents of file \"{}\"", FILE_PATH));
 
-    let find_instructions_regex = Regex::new(r"mul\(\d{1,3},\d{1,3}\)")
+    let find_instructions_regex = Regex::new(r"(don't\(\))|(do\(\))|(mul\(\d{1,3},\d{1,3}\))")
         .expect("Find Instructions regex could not be parsed.");
 
-    let multiply_instructions = find_instructions_regex.find_iter(&corrupted_data);
+    let instructions = find_instructions_regex.find_iter(&corrupted_data);
+
+    let mut valid_multiply_instructions: Vec<&str> = Vec::new();
+    let mut multiply_instructions_enabled = true;
+    for instruction in instructions {
+        let instruction_string = instruction.as_str();
+        match instruction_string {
+            DO_INSTRUCTION => multiply_instructions_enabled = true,
+            DONT_INSTRUCTION => multiply_instructions_enabled = false,
+            _ => {
+                if multiply_instructions_enabled {
+                    valid_multiply_instructions.push(instruction_string);
+                }
+            }
+        };
+    }
 
     let parse_multiply_instruction_regex = Regex::new(r"^mul\((?<digit_1>\d{1,3}),(?<digit_2>\d{1,3})\)$")
         .expect("Parse multiply instruction regex could not be parsed.");
 
-    let total: i32 = multiply_instructions.map(
+    let total: i32 = valid_multiply_instructions.into_iter().map(
         |instruction| {
-            let captures = parse_multiply_instruction_regex.captures(instruction.as_str())
-                .expect(&format!("Parse multiply instruction regex did not match \"{}\"", instruction.as_str()));
+            let captures = parse_multiply_instruction_regex.captures(instruction)
+                .expect(&format!("Parse multiply instruction regex did not match \"{}\"", instruction));
 
             let digit_1_string = captures.name("digit_1").unwrap().as_str();
             let digit_2_string = captures.name("digit_2").unwrap().as_str();
