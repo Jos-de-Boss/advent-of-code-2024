2f50bab8f96d57abdf87837c633d0f270b36d09c
Jos de Boss <s1149817@student.hsleiden.nl>
2024-12-03 19:38:24 +0100

    day-3: refactored puzzle 1 solution to separate finding and parsing logic which will make it easier to implement puzzle 2

--- day-3-mull-it-over/src/main.rs
+++ day-3-mull-it-over/src/main.rs
@@ -7,11 +7,19 @@ fn main() {
     let corrupted_data = fs::read_to_string(FILE_PATH)
         .expect(&format!("Could not read contents of file \"{}\"", FILE_PATH));
 
-    let multiply_instructions_regex = Regex::new(r"mul\((?<digit_1>\d{1,3}),(?<digit_2>\d{1,3})\)")
-        .expect("Regex could not be parsed");
+    let find_instructions_regex = Regex::new(r"mul\(\d{1,3},\d{1,3}\)")
+        .expect("Find Instructions regex could not be parsed.");
 
-    let total: i32 = multiply_instructions_regex.captures_iter(&corrupted_data).map(
-        |captures| {
+    let multiply_instructions = find_instructions_regex.find_iter(&corrupted_data);
+
+    let parse_multiply_instruction_regex = Regex::new(r"^mul\((?<digit_1>\d{1,3}),(?<digit_2>\d{1,3})\)$")
+        .expect("Parse multiply instruction regex could not be parsed.");
+
+    let total: i32 = multiply_instructions.map(
+        |instruction| {
+            let captures = parse_multiply_instruction_regex.captures(instruction.as_str())
+                .expect(&format!("Parse multiply instruction regex did not match \"{}\"", instruction.as_str()));
+
             let digit_1_string = captures.name("digit_1").unwrap().as_str();
             let digit_2_string = captures.name("digit_2").unwrap().as_str();
 
